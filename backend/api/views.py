e2f640f5f8a7260e648cd3e3294fd1cea7d28498
ethanbond64 <ethanbond39@gmail.com>
2024-03-23 22:37:05 -0600

    route stubs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,37 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint
 
 views = Blueprint("views", __name__)
 
-@views.route('/')
+@views.route('/version')
 def index():
-    return render_template('index.html', ii=url_for('static', filename='pic.png'))
+    return {"version": "1.0.0"}, 200
+
+
+@views.route('/<share>/signup', methods=['POST'])
+def signup(share):
+    return None
+
+@views.route('/login', methods=['POST'])
+def login():
+    return None
+
+@views.route('/update/users/<int:id>', methods=['POST'])
+def update_user(id):
+    return None
+
+@views.route('/insert/entries', methods=['POST'])
+def insert_entry():
+    return None
+
+@views.route('/update/entries/<int:id>', methods=['POST'])
+def update_entry(id):
+    return None
+
+@views.route('/fetch/<table>', methods=['GET'])
+def fetch(table):
+    return None
+
+# POST to put passcode in the body for locked entries.
+@views.route('/fetch/entries/<int:id>', methods=['POST'])
+def fetch_entry(id):
+    return None

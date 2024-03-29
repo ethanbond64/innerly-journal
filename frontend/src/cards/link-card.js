a60e2748281d8602400daef9e3e5a173e361db7e
ethanbond64 <ethanbond39@gmail.com>
2024-03-28 20:18:38 -0600

    working!

--- frontend/src/cards/link-card.js
+++ frontend/src/cards/link-card.js
@@ -3,5 +3,9 @@ import { ImageCard } from "./image-card";
 
 
 export const LinkCard = ({ entry }) => {
-    return (<ImageCard entry={entry} />);
+    return (
+        <a href={entry.entry_data.link} rel={`noreferrer`} target="_blank">
+            <ImageCard entry={entry} />
+        </a>
+    );
 }

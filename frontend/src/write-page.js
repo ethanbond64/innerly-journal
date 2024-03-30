d6428abf63753e111435bd8260fc37c6d55b4f3d
ethanbond64 <ethanbond39@gmail.com>
2024-03-30 10:06:25 -0600

    unhide

--- frontend/src/write-page.js
+++ frontend/src/write-page.js
@@ -4,7 +4,7 @@ import "./entry.css";
 export const WritePage = () => {
     return (
         <>
-            <main style={{ height: `90vh`, padding: '0', margin: '0' }} className="container" >
+            <main style={{ height: `90vh`, padding: '0', marginBottom: '0' }} className="container" >
                     <div className="row text-center" style={{ height: '90%' }}>
                         <div className="col-md-2 hidden-sm hidden-xs text-left" >
                             <a href="/">
@@ -12,7 +12,7 @@ export const WritePage = () => {
                             </a>
                         </div>
                         <div className="col-lg-8 col-md-10" style={{ height: '97%' }}>
-                            <div id="session-details" className="writeto-display" style={{ textAlign: 'left', border: 'none', overflow: 'visible' }}>
+                            <div id="session-details" className="writeto-display" style={{ textAlign: 'left', border: 'none', overflow: 'visible', paddingTop: '50px' }}>
                                 <div>
                                     <a href="/home" className="btn btn-warning btn-block hidden-xl hidden-lg hidden-md hidden-sm" style={{ width: 'auto', float: 'left' }}>
                                         <b><i className="fa fa-chevron-left" aria-hidden="true"></i></b>
@@ -20,7 +20,7 @@ export const WritePage = () => {
                                     </a>
                                     <span className="disappear hidden-xs" style={{ padding: '12px', }}>Write about any thoughts, experiences, or ideas</span>
                                     <button id="submitbtn" type="submit" className="btn btn-info btn-block" style={{ width: 'auto', float: 'right', color: 'white', marginRight: '12px' }}>
-                                        <span className="nremove hidden-xs" style={{ display: 'none' }}>Save</span>
+                                        <span className="nremove hidden-xs" >Save</span>
                                         <span className="hidden-xl hidden-lg hidden-md hidden-sm">Save</span>
                                         <b><i className="fa fa-chevron-right" aria-hidden="true"></i></b>
                                     </button>

14f7f872e59ecfa612198edee188c1a066dd2b84
ethanbond64 <ethanbond39@gmail.com>
2024-04-03 21:15:41 -0600

    works

--- frontend/src/row.js
+++ frontend/src/row.js
@@ -8,55 +8,34 @@ import { LinkCard } from './cards/link-card.js';
 export const Row = ({ row, setImagePath }) =>  {
 
     const [entryGroups, setEntryGroups] = useState([]);
-    // const [cardGroups, setCardGroups] = useState([]);
 
     const replace = (entry, index) => {
         console.log('replace', entry, index);
         setEntryGroups((prev) => {
+
             let outerIndex = Math.floor(index / 3);
             let innerIndex = index % 3;
-
             let newGroups = [...prev];
             let newGroup = [...newGroups[outerIndex]];
+
             newGroup[innerIndex] = entry;
             newGroups[outerIndex] = newGroup;
-            return newGroups;
+
+            return padGroups(newGroups);
         });
-        // setEntries((prev) => {
-        //     let newEntries = [...prev];
-        //     if (index >= newEntries.length) { 
-        //         newEntries[index] = entry;
-        //     } else {
-        //         while (newEntries.length < index) {
-        //             newEntries.push({ entry_type: 'blank' });
-        //         }
-        //         newEntries.push(entry);
-        //     }
-        //     return newEntries;
-        // });
     };
 
     useEffect(() => {
-        let localEntries = [...row.entries];
-
-        if (localEntries.length === 0 || localEntries % 3 !== 0) {
-            let required = 3 - (localEntries.length % 3);
-            for (let i = 0; i < required; i++) {
-                localEntries.push({ entry_type: 'blank' });
-            }
-        }
 
         let group = []
         let groups = [];
         let blanksRequired = true;
+        let localEntries = [...row.entries];
 
         for (let i = 0; i < localEntries.length; i++) {
 
             let entry = localEntries[i];
-            
-            if (entry.entry_type === 'blank') {
-                blanksRequired = false;
-            }
+            blanksRequired = blanksRequired && entry.entry_type !== 'blank'
             
             if (group.length === 3) {
                 groups.push(group);
@@ -66,17 +45,18 @@ export const Row = ({ row, setImagePath }) =>  {
             group.push(entry);
         }
 
+        //
+        // Pad the final group with blanks if incomplete.
+        //
         if (group.length > 0 || blanksRequired) {
-            
             let blanks = 3 - group.length;
-
             for (let i = 0; i < blanks; i++) {
                 group.push({ entry_type: 'blank' });
             }
-
             groups.push(group);
         }
 
+        groups = padGroups(groups);
 
         setEntryGroups(groups);
     }, [row.entries]);
@@ -93,39 +73,6 @@ export const Row = ({ row, setImagePath }) =>  {
                 return <BlankCard datetime={row.date} replace={replace} />;
         }
     };
-
-    // useEffect(() => {
-
-    //     const createCard = (entry, replace) => {
-    //         switch (entry.entry_type) {
-    //             case "text":
-    //                 return <TextCard entry={entry} />;
-    //             case "file":
-    //                 return <ImageCard entry={entry} setPath={setImagePath} replace={replace} />;
-    //             case "link":
-    //                 return <LinkCard entry={entry} replace={replace} />;
-    //             default:  
-    //                 return <BlankCard datetime={row.date} replace={replace} />;
-    //         };
-    //     };
-
-    //     let localCards = entries.map((entry, i) => createCard(entry, (e) => replace(e, i)));
-    //     let hasBlankEntry = entries.some(entry => entry.entry_type === 'blank');
-    //     if (!hasBlankEntry) {
-    //         let blanksRequired = 3 - (localCards.length % 3);
-    //         if (localCards.length !== 0) {
-    //             console.log('local ents', entries);
-    //             console.log('blanks required', blanksRequired);
-    //             console.log('local cards len', localCards.length);
-    //         }
-    //         for (let i = 0; i < blanksRequired; i++) {
-    //             localCards.push(<BlankCard datetime={row.date} replace={e => replace(e, localCards.length)}/>);
-    //         }
-    //         console.log('local cards len', localCards.length);
-    //     }   
-
-    //     setCardGroups(Array.from({ length: Math.ceil(localCards.length / 3) }, (_, index) => localCards.slice(index * 3, index * 3 + 3)));
-    // }, [row.date, setImagePath, entries])
 
     return entryGroups.map((entries, i) => (
         <div className={`well owell`} style={{ marginBottom: "0px" }}>
@@ -146,4 +93,17 @@ export const Row = ({ row, setImagePath }) =>  {
             </div>
         </div>
     ));
+}
+
+const padGroups = (groups) => {
+    console.log('padGroups', groups);
+    let containsBlanks = groups.some((group) => group.some((entry) => entry.entry_type === 'blank'));
+    if (!containsBlanks) {
+        let newGroup = [];
+        for (let i = 0; i < 3; i++) {
+            newGroup.push({ entry_type: 'blank' });
+        }
+        groups.push(newGroup);
+    }
+    return groups;
 }
